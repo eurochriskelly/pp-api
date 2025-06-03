b2e611ba5d32ab20f618a165597c6ee37e78af81
Chris Kelly <eurochriskelly@gmail.com>
2025-06-03 11:27:25 +0200

    fix: move db config require inside non-mock block

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql");
 const { processArgs } = require("./lib/process-args");
-const { dbConf } = require("../config");
+// const { dbConf } = require("../config"); // Moved down
 const apiSetup = require("./api/index");
 const ARGS = processArgs(process.argv);
 
 const run = async () => {
   let db = null;
   if (!ARGS.useMock) {
+    const { dbConf } = require("../config"); // Require config only if not in mock mode
     db = mysql.createConnection(dbConf);
     db.connect((err) => {
       if (err) {
