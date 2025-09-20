b16b2d369e033f635adb495798dbf04ec6440bf9
Chris Kelly <eurochriskelly@gmail.com>
2025-09-20 17:37:56 +0200

    fix backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,16 +2,20 @@
 # switch to top git folder
 
 cd $(git rev-parse --show-toplevel)
-source ./gcp_env.sh
+if [ ! -f "./pp_env.sh" ];then
+  echo "missing env file"
+  exit 1
+fi
+source ./pp_env.sh
 today=$(date +"%Y%m%dT%H%M")
 bkpdir=backups/${today}
-mkdir $bkpdir 
 
-echo "gggg $GCP_MYSQL_ROOT"
+echo "$PP_PWD"
+mkdir -p $bkpdir
 
-mysqldump -u root -p"$GCP_MYSQL_ROOT" EuroTourno > ${bkpdir}/production-backup-${today}.sql
-mysqldump -u root -p"$GCP_MYSQL_ROOT" AccTourno > ${bkpdir}/acceptance-backup-${today}.sql
-mysqldump -u root -p"$GCP_MYSQL_ROOT" --no-data --skip-comments --compact --skip-add-drop-table --skip-tz-utc EuroTourno > ${bkpdir}/production-schema-${today}.sql
-mysqldump -u root -p"$GCP_MYSQL_ROOT" --no-data --skip-comments --compact --skip-add-drop-table --skip-tz-utc AccTourno > ${bkpdir}/acceptance-schema-${today}.sql
+mysqldump -u root -p"$PP_PWD" EuroTourno > ${bkpdir}/production-backup-${today}.sql
+mysqldump -u root -p"$PP_PWD" AccTourno > ${bkpdir}/acceptance-backup-${today}.sql
+mysqldump -u root -p"$PP_PWD" --no-data --skip-comments --compact --skip-add-drop-table --skip-tz-utc EuroTourno > ${bkpdir}/production-schema-${today}.sql
+mysqldump -u root -p"$PP_PWD" --no-data --skip-comments --compact --skip-add-drop-table --skip-tz-utc AccTourno > ${bkpdir}/acceptance-schema-${today}.sql
 echo "Backups and schemas in backups/$today"
 
