623274c4c097926ed401e81ba5215bb4e7f153ff
Chris Kelly <eurochriskelly@gmail.com>
2025-11-22 16:06:34 +0100

    fix

--- src/api/services/mocks/clubs.js
+++ src/api/services/mocks/clubs.js
@@ -1,7 +1,18 @@
 // Mock service for clubs
 const { II } = require('../../../lib/logging');
 
-const mockClubs = [
+const   mockClubs = [
+  { clubId: 1, clubName: 'Amherst FC' },
+  { clubId: 2, clubName: 'Amazon Warriors' },
+  { clubId: 3, clubName: 'American Eagles' },
+  { clubId: 4, clubName: 'Alpha Club' },
+  { clubId: 5, clubName: 'Amber Stars' },
+  { clubId: 6, clubName: 'Apex United' },
+  { clubId: 7, clubName: 'Arsenal Juniors' },
+  { clubId: 8, clubName: 'Atlanta Athletic' },
+  { clubId: 9, clubName: 'Aurora FC' },
+  { clubId: 10, clubName: 'Avengers SC' },
+],
   { clubId: 1, Name: 'Amherst FC' },
   { clubId: 2, Name: 'Amazon Warriors' },
   { clubId: 3, Name: 'American Eagles' },
